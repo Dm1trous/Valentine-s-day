4d4a26e835949f3d46a611a08c3411e2d042c831
Dm1trous <lopic9999lokoko@gmail.com>
2024-02-14 00:23:12 +0500

    six

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -8,7 +8,3 @@ btn.addEventListener('mouseenter', () => {
     btn.style.left = `${random(0, 90)}%`;
     btn.style.top = `${random(0, 80)}%`;
 });
-
-btn.addEventListener('click', () => {
-    alert('Congrats! You hit the button! What are you doing with your life? :D');
-});
